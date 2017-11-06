ff3b74cf5a1eba0ee7b1cb7d9cf37a3ec3c6b539
JesusBrito <jesus291196@gmail.com>
2017-11-06 14:21:07 -0600

    Agregando los forms de administrador y el login

---
+++ Farmacia/src/Utilidades/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilidades;
+
+/**
+ *
+ * @author jesus
+ */
+public class Persona {
+    protected String RFC;
+    protected String Nombre;
+    protected String ApPaterno;
+    protected String ApMaterno;
+    protected String Calle;
+    protected String Colonia;
+    protected Integer Numero;
+    protected String Ciudad;
+    protected String Delegacion;
+    protected Integer Cp;
+}
